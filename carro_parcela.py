ae10d9e31c6ba98f7c82f646378a4136d3c324d4
akasca <50596634+akasca@users.noreply.github.com>
2019-05-16 18:52:28 -0300

    Update carro_parcela.py
    changed the print part

--- carro_parcela.py
+++ carro_parcela.py
@@ -3,4 +3,4 @@ from math import ceil
 carrop = float(input('Quanto custa o carro? \n'))
 carropa = int(input('Em Quantos Meses Você Quer Parcelar o Carro? \n'))
 carropar = carrop / carropa
-print('Vai parcelar em {} meses, Cada Parcela Vai Ser R${}.'.format(carropa, ceil(carropar)))
+print(f'Você Parcelou em {carropa} vezes, A parcela é {carropar}, O Carro Custa {carrop}!')
